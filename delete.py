480d522623d5f3578bba8fc32ec0a087a8f7186d
FatBoy <1161111567@qq.com>
2024-07-24 03:21:00 +0900

    Initial commit

---
+++ delete.py
@@ -0,0 +1,20 @@
+import os
+
+def delete_non_jbeam_files(folder_path):
+    """删除所有非 .jbeam 文件"""
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if not filename.endswith('.jbeam'):
+                file_path = os.path.join(root, filename)
+                try:
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Error deleting file {file_path}: {e}")
+
+if __name__ == "__main__":
+
+    folder_path = r'D:\zh\vehicles\zh_cn\vehicles'
+
+
+    delete_non_jbeam_files(folder_path)
